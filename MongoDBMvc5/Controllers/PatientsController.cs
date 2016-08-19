4705c49cae02975548bbbb299edee8a6780f20d4
Sean <seon2you@hotmail.com>
2016-08-18 23:46:17 -0700

    added get with id

--- MongoDBMvc5/Controllers/PatientsController.cs
+++ MongoDBMvc5/Controllers/PatientsController.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using MongoDBMvc5.Models;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,27 @@ namespace MongoDBMvc5.Controllers
         public IEnumerable<Patient> Get()
         {
             return _patients.FindAll();
+        }
+
+        //public HttpResponseMessage Get(string id)
+        //{
+        //    var patient = _patients.FindOneById(ObjectId.Parse(id));
+
+        //    if(patient == null)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Patient not found");
+        //    }
+        //    return Request.CreateResponse(patient);
+        //}
+
+        public IHttpActionResult Get(string id)
+        {
+            var patient = _patients.FindOneById(ObjectId.Parse(id));
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return Ok(patient);
         }
     }
 }
